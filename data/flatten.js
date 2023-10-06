6f535596b1544cf757210da34ec611a44a2f0e95
Tom Gagliano <tgaglianoll@gmail.com>
2023-10-06 15:15:29 -0400

    solve flatten problem in javascript

--- data/flatten.js
+++ data/flatten.js
@@ -3,3 +3,14 @@
 // Input: {“a” => 1, “b” => 2, “c” => 3, “d” => 4}
 // Output: [“a”, 1, “b”, 2, “c”, 3, “d”, 4]
 
+function flatten(hash) {
+  let newArray = [];
+
+  Object.keys(hash).forEach(function (key) {
+    newArray.push(key, hash[key]);
+  });
+
+  return newArray;
+}
+
+console.log(flatten({ a: 1, b: 2, c: 3, d: 4 }));
