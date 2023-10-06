23ba313af5306fe80c726ec0103b144b93e4f520
Tom Gagliano <tgaglianoll@gmail.com>
2023-10-06 15:26:14 -0400

    solve flatten problem in python

--- data/flatten.py
+++ data/flatten.py
@@ -3,3 +3,13 @@
 # Input: {“a” => 1, “b” => 2, “c” => 3, “d” => 4}
 # Output: [“a”, 1, “b”, 2, “c”, 3, “d”, 4]
 
+def flatten(hash):
+  newArray = []
+
+  for key in hash:
+    newArray.append(key)
+    newArray.append(hash[key])
+
+  return newArray
+
+print(flatten({ "a": 1, "b": 2, "c": 3, "d": 4 }))
