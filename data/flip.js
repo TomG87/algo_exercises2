0429631209a76e120934027a5d5a243b264e1c72
Tom Gagliano <tgaglianoll@gmail.com>
2023-10-09 18:59:31 -0400

    solve flip question in javascript

--- data/flip.js
+++ data/flip.js
@@ -2,3 +2,14 @@
 
 // Input: {"a" => 1, "b" => 2, "c" => 3}
 // Output: {1 => "a", 2 => "b", 3 => "c"}
+
+function flip(hash) {
+  let newHash = {};
+
+  Object.keys(hash).forEach(function (key) {
+    newHash[hash[key]] = key;
+  });
+  return newHash;
+}
+
+console.log(flip({ a: 1, b: 2, c: 3 }));
