96f304b345599942a6d3f9231c51c6a98bde38fe
Tom Gagliano <tgaglianoll@gmail.com>
2023-10-09 19:02:58 -0400

    solve flip question in python

--- data/flip.py
+++ data/flip.py
@@ -2,3 +2,14 @@
 
 # Input: {"a" => 1, "b" => 2, "c" => 3}
 # Output: {1 => "a", 2 => "b", 3 => "c"}
+
+def flip(hash):
+  newHash = {}
+
+  for key in hash:
+    newHash[hash[key]] = key
+
+  
+  return newHash
+  
+print(flip({ "a": 1, "b": 2, "c": 3 }))
