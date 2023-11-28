8d4f37cd6b4936fb6cd6dce48398f604fb847e78
Tom Gagliano <tgaglianoll@gmail.com>
2023-11-28 16:47:38 -0500

    solve duplicate problem in javascript

--- linear/duplicate.js
+++ linear/duplicate.js
@@ -5,6 +5,20 @@
 // Input: [5, 2, 9, 7, 2, 6]
 // Output: 2
 
-function duplicate(array)
+function duplicate(array) {
+  let i = 0;
 
+  while (i < array.length - 1) {
+    j = i + 1;
 
+    while (j < array.length) {
+      if (array[i] === array[j]) {
+        return array[i];
+      }
+      j += 1;
+    }
+    i += 1;
+  }
+}
+
+console.log(duplicate([5, 2, 9, 7, 2, 6]));
