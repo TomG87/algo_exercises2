0efda1d546fe3548292e1592c7a3b7169962d9bd
Tom Gagliano <tgaglianoll@gmail.com>
2023-12-12 20:30:44 -0500

    solve remove first and last character problem in python

--- linear/remove.py
+++ linear/remove.py
@@ -1,1 +1,13 @@
 # Create a function that removes the first and last characters from a string.
+
+def removeChar(string):
+
+  newString = ""
+
+  if len(string) <= 2:
+    return string
+  else:
+    newString =string[1:-1]
+  return newString
+
+print(removeChar("hello"))
