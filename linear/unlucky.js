864cf81283627e2acb39d28e80448c25aad14606
Tom Gagliano <tgaglianoll@gmail.com>
2023-12-14 20:20:16 -0500

    solve unlucky 13 problem in javascript

--- linear/unlucky.js
+++ linear/unlucky.js
@@ -1,1 +1,14 @@
 // Given a sorted array of numbers, remove any numbers that are divisible by 13. Return the amended array.
+
+function unlucky(array) {
+  newHash = [];
+
+  array.forEach(function (number) {
+    if (number % 13 !== 0) {
+      newHash.push(number);
+    }
+  });
+  return newHash;
+}
+
+console.log(unlucky([24, 316, 393, 458, 1279]));
