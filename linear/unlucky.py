fa417eb8613f5c00431ca8c771169f0bd5faeb25
Tom Gagliano <tgaglianoll@gmail.com>
2023-12-14 20:23:01 -0500

    solve unlucky 13 problem in python

--- linear/unlucky.py
+++ linear/unlucky.py
@@ -1,1 +1,13 @@
 # Given a sorted array of numbers, remove any numbers that are divisible by 13. Return the amended array.
+
+def unlucky(array):
+
+  newHash = []
+
+  for number in array:
+    if number % 13 != 0:
+      newHash.append(number)
+  
+  return newHash
+
+print(unlucky([24, 316, 393, 458, 1279]))
