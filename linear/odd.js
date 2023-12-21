25e2af3054f8ea812830cbc16b9b0bd57192d6a3
Tom Gagliano <tgaglianoll@gmail.com>
2023-12-20 19:35:13 -0500

    solve odd sum problem in javascript

--- linear/odd.js
+++ linear/odd.js
@@ -1,1 +1,16 @@
-Create a function which returns the total of all odd numbers up to and including n. n will be given as an odd number.
+// Create a function which returns the total of all odd numbers up to and including n. n will be given as an odd number.
+
+function odd(array) {
+  let i = 0;
+  let sum = 0;
+
+  while (i < array.length) {
+    if (array[i] % 2 !== 0) {
+      sum += array[i];
+    }
+    i += 1;
+  }
+  return sum;
+}
+
+console.log(odd([1, 2, 5, 4, 6, 7, 8, 9, 13]));
