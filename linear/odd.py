3a765b619436e9aed7567b063ac8e3d7354f1aee
Tom Gagliano <tgaglianoll@gmail.com>
2023-12-20 19:38:27 -0500

    solve odd sum problem in python

--- linear/odd.py
+++ linear/odd.py
@@ -1,1 +1,15 @@
-Create a function which returns the total of all odd numbers up to and including n. n will be given as an odd number.
+# Create a function which returns the total of all odd numbers up to and including n. n will be given as an odd number.
+
+def odd(array):
+
+  i = 0
+  sum = 0
+
+  while i < len(array):
+    if array[i] % 2 != 0:
+      sum += array[i]
+    i += 1
+  
+  return sum
+
+print(odd([1,2,5,4,6,7,8,9,13]))
