8dcdd9e7ed6275e47082b784dc60275e7afcbf1f
Tom Gagliano <tgaglianoll@gmail.com>
2024-01-09 19:58:21 -0500

    solve bomb question in ruby

--- linear/bomb.rb
+++ linear/bomb.rb
@@ -1,1 +1,12 @@
 # Create a function that finds the word "bomb" in the given string (not case sensitive). If found, return "Duck!!!", otherwise return "There is no bomb, relax.".
+
+def bomb(string)
+
+  if string.downcase.include?("bomb")
+    return "Duck!!!"
+  else
+    return "There is no bomb, relax."
+  end
+end
+
+p bomb("There is a bomb")
